646109bc7b07f588499b66124caf101aab626ea5
kgurmeet <131999481+kgurmeet@users.noreply.github.com>
2023-07-18 15:11:58 +0530

    Add files via upload

---
+++ Func1.java
@@ -0,0 +1,15 @@
+package Function;
+
+public class Func1 {
+    static int sum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=sum(a,b);
+        System.out.println("The sum is "+c);
+
+    }
+    
+}
